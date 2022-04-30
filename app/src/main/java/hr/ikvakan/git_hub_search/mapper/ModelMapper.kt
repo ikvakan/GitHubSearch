0c95d0ca655f0e363b5fed2a65b5e62692eabf71
ikvakan <37423910+ikvakan@users.noreply.github.com>
2022-04-30 20:43:34 +0200

    Removed comments

--- app/src/main/java/hr/ikvakan/git_hub_search/mapper/ModelMapper.kt
+++ app/src/main/java/hr/ikvakan/git_hub_search/mapper/ModelMapper.kt
@@ -1,9 +1,5 @@
 package hr.ikvakan.git_hub_search.mapper
 
-//interface ModelMapper<EntityItem,DomainModel> {
-//    fun mapFromEntityItemToDomainModel(entityItem: EntityItem) : DomainModel
-//}
-
 interface ModelMapper<EntityItem,DomainModel> {
     fun mapFromEntityItemToDomainModel(entityItem: EntityItem) : DomainModel
 }
