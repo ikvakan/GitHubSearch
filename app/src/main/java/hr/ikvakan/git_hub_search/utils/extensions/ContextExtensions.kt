ed17d0228e5c71b6104bf0509325ce2a8af843d4
ikvakan <37423910+ikvakan@users.noreply.github.com>
2022-04-30 14:47:25 +0200

    Added vertical margin to pager layout

--- app/src/main/java/hr/ikvakan/git_hub_search/utils/extensions/ContextExtensions.kt
+++ app/src/main/java/hr/ikvakan/git_hub_search/utils/extensions/ContextExtensions.kt
@@ -10,24 +10,11 @@ import android.widget.Toast
 import androidx.preference.PreferenceManager
 
 
-private const val QUERY="hr.decode.decode_demo.utils_query"
 
 inline fun<reified T : Activity> Context.startActivity() = Intent(this,T::class.java).also { startActivity(it) }
 
 inline fun <reified T : Activity> Context.startActivity(key:String,value:String) =
     startActivity(Intent(this, T::class.java).apply { putExtra(key,value)})
-
-inline fun<reified T : BroadcastReceiver> Context.sendBroadcast() = sendBroadcast(Intent(this,T::class.java))
-
-fun Context.setBooleanPreference(key:String, value: Boolean) =
-    PreferenceManager.getDefaultSharedPreferences(this)
-        .edit()
-        .putBoolean(key,value)
-        .apply()
-
-fun Context.getBooleanPreference(key:String) =
-    PreferenceManager.getDefaultSharedPreferences(this)
-        .getBoolean(key,false )
 
 fun Context.isOnline() :Boolean {
     val connectivityManager= getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
